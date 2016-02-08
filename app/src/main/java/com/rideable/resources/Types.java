b533cb2f03d29bab9e8242e051d7e5cbfe99521e
Jay Gadi <jay.gadi0@gmail.com>
2016-02-08 03:50:04 -0500

    Use 10.0.2.2 for emulator URL

--- app/src/main/java/com/rideable/resources/Types.java
+++ app/src/main/java/com/rideable/resources/Types.java
@@ -6,9 +6,9 @@ import com.rideable.model.User;
  * Created by Jay on 11/19/2015.
  */
 public class Types {
-    public static final String USER_URL = "http://192.168.2.29:8080/Rideable/ride/user";
-    public static final String AD_URL = "http://192.168.2.29:8080/Rideable/ride/ad";
-    public static final String MESSAGE_URL = "http://192.168.2.29:8080/Rideable/ride/message";
+    public static final String USER_URL = "http://10.0.2.2:8080/Rideable/ride/user";
+    public static final String AD_URL = "http://10.0.2.2:8080/Rideable/ride/ad";
+    public static final String MESSAGE_URL = "http://10.0.2.2:8080/Rideable/ride/message";
     public static User aUser = null;
     public static final String PROJECT_NUMBER = "415028633094";
 
